5f3e466c477291f598ff3bcd81297512606d5c7c
DEEPASUNDARAM <deepaksenthil005@gmail.com>
2026-03-11 09:24:05 +0530

    some design & backend

--- Frontend/src/Home.jsx
+++ Frontend/src/Home.jsx
@@ -15,15 +15,20 @@ function Home() {
 
   return (
     <div className="container">
-      <h2>Welcome to Home Page 🎉</h2>
+      <h2>Welcome to Home Page </h2>
       {photo ? (
         <div className="avatar-wrap">
           <img className="avatar" src={photo} alt="Profile" />
         </div>
-      ) :
+      ) : (
         <div className="avatar-wrap">
-          <img className="avatar" src="https://static.vecteezy.com/system/resources/previews/021/548/095/original/default-profile-picture-avatar-user-avatar-icon-person-icon-head-icon-profile-picture-icons-default-anonymous-user-male-and-female-businessman-photo-placeholder-social-network-avatar-portrait-free-vector.jpg" alt="Profile" />
-        </div>}
+          <img
+            className="avatar"
+            src="https://static.vecteezy.com/system/resources/previews/021/548/095/original/default-profile-picture-avatar-user-avatar-icon-person-icon-head-icon-profile-picture-icons-default-anonymous-user-male-and-female-businessman-photo-placeholder-social-network-avatar-portrait-free-vector.jpg"
+            alt="Profile"
+          />
+        </div>
+      )}
       {username ? <h3>Hi, {username}</h3> : null}
       <h3>Your Role: {role}</h3>
       <button onClick={handleLogout}>Logout</button>
